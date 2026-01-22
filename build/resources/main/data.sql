e1b7ca660ec6afa76bc5aab8988f6f02cda20f43
Vitus-at-work <vitus.kaminski@edu.tbz.ch>
2026-01-22 15:43:36 +0100

    Add user_role to all users and add new mockup data

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -4,26 +4,26 @@
 
 insert into users (id, email,first_name,last_name, password)
 values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'admin@example.com', 'James','Bond', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6' ), -- Password: 1234
-('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'user@example.com', 'Tyler','Durden', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6') -- Password: 1234
- ON CONFLICT DO NOTHING;
+       ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'user@example.com', 'Tyler','Durden', '$2a$10$TM3PAYG3b.H98cbRrHqWa.BM7YyCqV92e/kUTBfj85AjayxGZU7d6') -- Password: 1234
+ON CONFLICT DO NOTHING;
 
 
 --ROLES
 INSERT INTO role(id, name)
 VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', 'DEFAULT'),
-('ab505c92-7280-49fd-a7de-258e618df074', 'ADMIN'),
-('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'USER'),
-('78ebc27a-eddf-46da-b498-d4794e3d2c9c', 'OWNER'),
-('5aaa56bd-66a2-4f93-9926-108371df2833', 'COLLABORATOR'),
-('ef421859-4347-4ed6-8cc3-2acc69e40fbc', 'ATTENDEE')
+       ('ab505c92-7280-49fd-a7de-258e618df074', 'ADMIN'),
+       ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'USER'),
+       ('78ebc27a-eddf-46da-b498-d4794e3d2c9c', 'OWNER'),
+       ('5aaa56bd-66a2-4f93-9926-108371df2833', 'COLLABORATOR'),
+       ('ef421859-4347-4ed6-8cc3-2acc69e40fbc', 'ATTENDEE')
 
 ON CONFLICT DO NOTHING;
 
 --AUTHORITIES
 INSERT INTO authority(id, name)
 VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'USER_CREATE'),
-('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'USER_READ'),
-('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DEACTIVATE')
+       ('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'USER_READ'),
+       ('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DEACTIVATE')
 ON CONFLICT DO NOTHING;
 INSERT INTO authority(id, name) VALUES
                                     ('a1c942db-a275-43f8-bdd6-d048c21bf5ab', 'EVENT_CREATE'),
@@ -46,14 +46,14 @@ values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c
        ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'd29e709c-0ff1-4f4c-a7ef-09f656c390f1'),
        ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'),
        ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'c6aee32d-8c35-4481-8b3e-a876a39b0c02')
- ON CONFLICT DO NOTHING;
+ON CONFLICT DO NOTHING;
 
 --assign authorities to roles
 INSERT INTO role_authority(role_id, authority_id)
 VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
-('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
-('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab')
- ON CONFLICT DO NOTHING;
+       ('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
+       ('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab')
+ON CONFLICT DO NOTHING;
 -- =========================
 -- USERS
 -- =========================
@@ -123,4 +123,23 @@ INSERT INTO users_role (users_id, role_id)
 VALUES
     ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'ab505c92-7280-49fd-a7de-258e618df074'), -- ADMIN
     ('0d8fa44c-54fd-4cd0-ace9-2a7da57992de', 'ef421859-4347-4ed6-8cc3-2acc69e40fbc')  -- ATTENDEE
+ON CONFLICT DO NOTHING;
+-- EVENT_CREATE der USER Rolle zuweisen
+INSERT INTO role_authority (role_id, authority_id)
+VALUES (
+           'c6aee32d-8c35-4481-8b3e-a876a39b0c02', -- USER
+           'a1c942db-a275-43f8-bdd6-d048c21bf5ab'  -- EVENT_CREATE
+       )
+ON CONFLICT DO NOTHING;
+INSERT INTO users_role (users_id, role_id)
+VALUES (
+           '0d8fa44c-54fd-4cd0-ace9-2a7da57992de', -- user@example.com
+           'c6aee32d-8c35-4481-8b3e-a876a39b0c02'  -- USER
+       )
+ON CONFLICT DO NOTHING;
+INSERT INTO users_role (users_id, role_id)
+SELECT
+    u.id,
+    'c6aee32d-8c35-4481-8b3e-a876a39b0c02' -- USER
+FROM users u
 ON CONFLICT DO NOTHING;
